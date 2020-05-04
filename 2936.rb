42b80fb42a924dcac6a16f65f7083279e133caef
Diego Linhares <diegomlinhares@gmail.com>
2020-05-04 14:00:27 -0300

    More one problem solved

---
+++ 2936.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+curupira = gets.to_i
+boitata = gets.to_i
+boto = gets.to_i
+mapinguari = gets.to_i
+iara = gets.to_i
+
+total = curupira * 300 +
+        boitata * 1500 +
+        boto * 600 +
+        mapinguari * 1000 +
+        iara * 150 + 225
+
+puts total
